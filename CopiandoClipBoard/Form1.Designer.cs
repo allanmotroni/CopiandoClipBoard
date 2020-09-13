958e6633e7f33c17f0b73d0f5e1eb81c5b0a8714
Allan Rodrigo Motroni <allan.motroni@embraport.net>
2020-09-13 10:53:00 -0300

    Add project files.

---
+++ CopiandoClipBoard/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace CopiandoClipBoard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.checkBoxSempreVisivel = new System.Windows.Forms.CheckBox();
+            this.checkBoxVerificarAreaTransferencia = new System.Windows.Forms.CheckBox();
+            this.richTextBoxTexto = new System.Windows.Forms.RichTextBox();
+            this.timerAreaTransferencia = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelQuantidade = new System.Windows.Forms.Label();
+            this.checkBoxQuebrarLinha = new System.Windows.Forms.CheckBox();
+            this.linkLabelLimparAreaTransferencia = new System.Windows.Forms.LinkLabel();
+            this.btnExportarTxt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkBoxSempreVisivel
+            // 
+            this.checkBoxSempreVisivel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.checkBoxSempreVisivel.AutoSize = true;
+            this.checkBoxSempreVisivel.Location = new System.Drawing.Point(214, 300);
+            this.checkBoxSempreVisivel.Name = "checkBoxSempreVisivel";
+            this.checkBoxSempreVisivel.Size = new System.Drawing.Size(96, 17);
+            this.checkBoxSempreVisivel.TabIndex = 5;
+            this.checkBoxSempreVisivel.Text = "Sempre visível";
+            this.checkBoxSempreVisivel.UseVisualStyleBackColor = true;
+            this.checkBoxSempreVisivel.CheckedChanged += new System.EventHandler(this.checkBoxSempreVisivel_CheckedChanged);
+            // 
+            // checkBoxVerificarAreaTransferencia
+            // 
+            this.checkBoxVerificarAreaTransferencia.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.checkBoxVerificarAreaTransferencia.AutoSize = true;
+            this.checkBoxVerificarAreaTransferencia.Location = new System.Drawing.Point(12, 300);
+            this.checkBoxVerificarAreaTransferencia.Name = "checkBoxVerificarAreaTransferencia";
+            this.checkBoxVerificarAreaTransferencia.Size = new System.Drawing.Size(172, 17);
+            this.checkBoxVerificarAreaTransferencia.TabIndex = 3;
+            this.checkBoxVerificarAreaTransferencia.Text = "Verificar Área de Transferência";
+            this.checkBoxVerificarAreaTransferencia.UseVisualStyleBackColor = true;
+            this.checkBoxVerificarAreaTransferencia.CheckedChanged += new System.EventHandler(this.checkBoxVerificarAreaTransferencia_CheckedChanged);
+            // 
+            // richTextBoxTexto
+            // 
+            this.richTextBoxTexto.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBoxTexto.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBoxTexto.Location = new System.Drawing.Point(2, 1);
+            this.richTextBoxTexto.Name = "richTextBoxTexto";
+            this.richTextBoxTexto.Size = new System.Drawing.Size(318, 259);
+            this.richTextBoxTexto.TabIndex = 0;
+            this.richTextBoxTexto.Text = "";
+            this.richTextBoxTexto.WordWrap = false;
+            this.richTextBoxTexto.TextChanged += new System.EventHandler(this.richTextBoxTexto_TextChanged);
+            // 
+            // timerAreaTransferencia
+            // 
+            this.timerAreaTransferencia.Tick += new System.EventHandler(this.timerAreaTransferencia_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 263);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Quantidade de linhas";
+            // 
+            // labelQuantidade
+            // 
+            this.labelQuantidade.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelQuantidade.AutoSize = true;
+            this.labelQuantidade.Enabled = false;
+            this.labelQuantidade.Location = new System.Drawing.Point(125, 263);
+            this.labelQuantidade.Name = "labelQuantidade";
+            this.labelQuantidade.Size = new System.Drawing.Size(13, 13);
+            this.labelQuantidade.TabIndex = 4;
+            this.labelQuantidade.Text = "0";
+            // 
+            // checkBoxQuebrarLinha
+            // 
+            this.checkBoxQuebrarLinha.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.checkBoxQuebrarLinha.AutoSize = true;
+            this.checkBoxQuebrarLinha.Location = new System.Drawing.Point(12, 323);
+            this.checkBoxQuebrarLinha.Name = "checkBoxQuebrarLinha";
+            this.checkBoxQuebrarLinha.Size = new System.Drawing.Size(89, 17);
+            this.checkBoxQuebrarLinha.TabIndex = 4;
+            this.checkBoxQuebrarLinha.Text = "Quebrar linha";
+            this.checkBoxQuebrarLinha.UseVisualStyleBackColor = true;
+            this.checkBoxQuebrarLinha.CheckedChanged += new System.EventHandler(this.checkBoxQuebrarLinha_CheckedChanged);
+            // 
+            // linkLabelLimparAreaTransferencia
+            // 
+            this.linkLabelLimparAreaTransferencia.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.linkLabelLimparAreaTransferencia.AutoSize = true;
+            this.linkLabelLimparAreaTransferencia.Location = new System.Drawing.Point(169, 263);
+            this.linkLabelLimparAreaTransferencia.Name = "linkLabelLimparAreaTransferencia";
+            this.linkLabelLimparAreaTransferencia.Size = new System.Drawing.Size(141, 13);
+            this.linkLabelLimparAreaTransferencia.TabIndex = 2;
+            this.linkLabelLimparAreaTransferencia.TabStop = true;
+            this.linkLabelLimparAreaTransferencia.Text = "Limpar área de transferência";
+            this.linkLabelLimparAreaTransferencia.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelLimparAreaTransferencia_LinkClicked);
+            // 
+            // btnExportarTxt
+            // 
+            this.btnExportarTxt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnExportarTxt.Location = new System.Drawing.Point(214, 323);
+            this.btnExportarTxt.Name = "btnExportarTxt";
+            this.btnExportarTxt.Size = new System.Drawing.Size(82, 23);
+            this.btnExportarTxt.TabIndex = 6;
+            this.btnExportarTxt.Text = "Exportar TXT";
+            this.btnExportarTxt.UseVisualStyleBackColor = true;
+            this.btnExportarTxt.Click += new System.EventHandler(this.btnExportarTxt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(322, 349);
+            this.Controls.Add(this.btnExportarTxt);
+            this.Controls.Add(this.linkLabelLimparAreaTransferencia);
+            this.Controls.Add(this.checkBoxQuebrarLinha);
+            this.Controls.Add(this.labelQuantidade);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBoxTexto);
+            this.Controls.Add(this.checkBoxVerificarAreaTransferencia);
+            this.Controls.Add(this.checkBoxSempreVisivel);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Copiando para Área de Transferência";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBoxSempreVisivel;
+        private System.Windows.Forms.CheckBox checkBoxVerificarAreaTransferencia;
+        private System.Windows.Forms.RichTextBox richTextBoxTexto;
+        private System.Windows.Forms.Timer timerAreaTransferencia;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelQuantidade;
+        private System.Windows.Forms.CheckBox checkBoxQuebrarLinha;
+        private System.Windows.Forms.LinkLabel linkLabelLimparAreaTransferencia;
+        private System.Windows.Forms.Button btnExportarTxt;
+    }
+}
+
---
+++ CopiandoClipBoard/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CopiandoClipBoard
+{
+    public partial class Form1 : Form
+    {
+        private bool verificarAreaTranferencia;
+        public bool VerificarAreaTransferencia
+        {
+            get { return this.verificarAreaTranferencia; }
+            set
+            {
+                this.verificarAreaTranferencia = value;
+                timerAreaTransferencia.Enabled = value;
+            }
+        }
+
+        private string textoAreaTransferencia;
+        public string TextoAreaTransferencia
+        {
+            get { return this.textoAreaTransferencia; }
+            set { this.textoAreaTransferencia = value; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBoxSempreVisivel_CheckedChanged(object sender, EventArgs e)
+        {
+            this.TopMost = checkBoxSempreVisivel.Checked;
+        }
+
+        private void checkBoxVerificarAreaTransferencia_CheckedChanged(object sender, EventArgs e)
+        {
+            this.VerificarAreaTransferencia = checkBoxVerificarAreaTransferencia.Checked;
+        }
+
+        private void timerAreaTransferencia_Tick(object sender, EventArgs e)
+        {
+            string textoAtual = Clipboard.GetText();
+
+            if (!string.IsNullOrEmpty(textoAtual))
+            {
+                if (TextoAreaTransferencia != textoAtual)
+                {
+                    AdicionarTexto(textoAtual);
+                    TextoAreaTransferencia = textoAtual;
+                }
+            }
+        }
+
+        private void AdicionarTexto(string textoAtual)
+        {
+            richTextBoxTexto.Text += textoAtual + Environment.NewLine;
+
+        }
+
+        private void richTextBoxTexto_TextChanged(object sender, EventArgs e)
+        {
+            AtualizarContegemLinhas();
+        }
+
+        private void AtualizarContegemLinhas()
+        {
+            int linhas = richTextBoxTexto.Lines.Length;
+            if (linhas <= 1)
+            {
+                labelQuantidade.Text = (richTextBoxTexto.Lines.Length).ToString();
+            }
+            else if (linhas > 1)
+            {
+                if (richTextBoxTexto.Lines[linhas - 1].Length > 0)
+                    labelQuantidade.Text = (richTextBoxTexto.Lines.Length).ToString();
+                else
+                    labelQuantidade.Text = (richTextBoxTexto.Lines.Length - 1).ToString();
+            }
+        }
+
+        private void checkBoxQuebrarLinha_CheckedChanged(object sender, EventArgs e)
+        {
+            richTextBoxTexto.WordWrap = checkBoxQuebrarLinha.Checked;
+            richTextBoxTexto.Refresh();
+        }
+
+        private void linkLabelLimparAreaTransferencia_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Clipboard.Clear();
+        }
+
+        private void btnExportarTxt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (richTextBoxTexto.Lines.Count() == 0) throw new Exception("A lista está vazia!");
+
+                SaveFileDialog f = new SaveFileDialog();
+                f.Filter = "Arquivo Texto (.txt)| .txt";
+                f.FileName = string.Empty;                
+
+                if (f.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    string caminho = f.FileName;
+                    using (System.IO.StreamWriter sw = new System.IO.StreamWriter(caminho))
+                    {
+                        foreach (string linha in richTextBoxTexto.Lines)
+                        {
+                            sw.WriteLine(linha);
+                        }
+                        sw.Close();
+                    }
+                    MessageBox.Show("Arquivo salvo com sucesso!", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
